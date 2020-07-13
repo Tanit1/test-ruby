43455f206076f0f78d162178fa8e0d163d63ccc1
Gogs <gogs@fake.local>
2020-07-13 23:26:31 +0200

    Init rspec

--- lib/06_pig_latin.rb
+++ lib/06_pig_latin.rb
@@ -8,43 +8,17 @@ def translate(string)
    # POUR CHAQUE MOT CONTENU DANS string
    string.each_with_index do |word, i|
       translation = ''
-      qu = false
 
       # SI string COMMENCE PAR UNE VOYELLE => AJOUTE ay À LA FIN DU MOT
       if vowels.include? word[0]
          translation = word + 'ay'
          result.push(translation)
       # SINON DÉCOUPE LE MOT
-      else
-         word  = word.split('')
-         count = 0
-
-         # POUR CHAQUE LETTRE CONTENU PAR MOT
-         word.each_with_index do |char, index|
-            if vowels.include? char
-               # SI LE MOT COMMENCE PAR 'qu'
-               if char == 'u' and translation[-1] == 'q'
-                  qu = true
-                  translation = string[i][count + 1..string[i].length] + translation + 'uay'
-                  result.push(translation)
-                  next
-               end
-               break
-            else
-               translation += char
-               count += 1
-            end
-         end
-
-         # S'IL N'Y A PAS DE "qu"
-         if not qu
-            translation = string[i][count..string[i].length] + translation + 'ay'
-            result.push(translation)
-         end
+      
       end
    end
 
    return result.join(' ').to_s
 end
 
-# puts translate('apple')
+puts translate('apple')
